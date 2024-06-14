1b427fdec32957693a6f25618ba03f2322e4603a
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-06-14 09:56:28 +0200

    build: install flyway and postgres drivers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,9 @@ application {
 }
 
 dependencies {
+  implementation("org.flywaydb:flyway-core:10.15.0")
+  implementation("org.flywaydb:flyway-database-postgresql:10.15.0")
+  implementation("org.postgresql:postgresql:42.6.0")
   implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
   testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
