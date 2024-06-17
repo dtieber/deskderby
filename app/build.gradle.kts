dtieber/biceur

feat: initialize spring boot application

app/build.gradle.kts
@@ -1,3 +1,7 @@
+application {
+  mainClass.set("com.biceur.App")
+}
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
   testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")

app/src/main/java/com/biceur/App.java
@@ -1,0 +1,11 @@
+package com.biceur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}

dtieber/deskderby

app/build.gradle.kts
@@ -1,3 +1,7 @@
+application {
+  mainClass.set("com.dd.App")
+}
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
   testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")

app/src/main/java/com/dd/App.java
@@ -1,0 +1,11 @@
+package com.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}