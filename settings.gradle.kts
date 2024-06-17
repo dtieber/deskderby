dtieber/biceur

build: init gradle project

settings.gradle.kts
@@ -1,0 +1,2 @@
+rootProject.name = "biceur"
+include("app")

dtieber/deskderby

settings.gradle.kts
@@ -1,0 +1,2 @@
+rootProject.name = "deskderby"
+include("app")